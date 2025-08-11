e7f8ac086bf03de5aded2d55f16f8ee5ea6bae7b
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-08-12 00:20:21 +0530

    created coding app

---
+++ convex/http.ts
@@ -0,0 +1,63 @@
+import { httpRouter } from "convex/server";
+import { httpAction } from "./_generated/server";
+import { Webhook } from "svix";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { api } from "./_generated/api";
+
+const http = httpRouter();
+http.route({
+  path: "/clerk-webhook",
+  method: "POST",
+
+  handler: httpAction(async (ctx, request) => {
+    const webhookSecret = process.env.CLERK_WEBHOOK_SECRET!;
+    if (!webhookSecret) {
+      throw new Error("No webhook secret found");
+    }
+
+    const svix_id = request.headers.get("svix-id");
+    const svix_signature = request.headers.get("svix-signature");
+    const svix_timestamp = request.headers.get("svix-timestamp");
+
+    if (!svix_id || !svix_signature || !svix_timestamp) {
+      return new Response("No headers available", {
+        status: 400,
+      });
+    }
+
+    const payload = await request.json();
+    const body = JSON.stringify(payload);
+
+    const wh = new Webhook(webhookSecret);
+    let evt: WebhookEvent;
+
+    try {
+      evt = wh.verify(body, {
+        "svix-id": svix_id,
+        "svix-timestamp": svix_timestamp,
+        "svix-signature": svix_signature,
+      }) as WebhookEvent;
+    } catch (err) {
+      console.error("Error Veryfying Webhook", err);
+      return new Response("Error Occured", { status: 400 });
+    }
+
+    const eventType = evt.type;
+    if (eventType === "user.created") {
+      const { id, email_addresses, first_name, last_name } = evt.data;
+
+      const email = email_addresses[0]?.email_address || "";
+      const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+      // TODO: Implement user creation logic here
+      try{
+        await ctx.runMutation(api.users.syncUser,{userId : id, email, name, });
+      }catch(err){
+        return new Response("Error Creating User")
+      }
+    }
+    return new Response("Webhook Processed successfully", { status: 200 });
+  }),
+});
+
+export default http;
